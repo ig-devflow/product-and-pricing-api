212325f3f908ae13102ed6b57e6954ea654b9235
igor.p <igor.parfianets@gmail.com>
2026-05-26 15:49:45 +0200

    Remove redundant lines

--- ProductsAndPricingNew.Persistence/Scripts/Seeders/CentreContactType.sql
+++ ProductsAndPricingNew.Persistence/Scripts/Seeders/CentreContactType.sql
@@ -1,8 +1,4 @@
-SET IDENTITY_INSERT [PricingRef].[CentreContactType] ON;
-
 INSERT INTO [PricingRef].[CentreContactType] ([Id], [Name], [IsDeleted])
 VALUES
     (1, N'Centre Director',    0),
     (2, N'Director of Studies', 0);
-
-SET IDENTITY_INSERT [PricingRef].[CentreContactType] OFF;
--- ProductsAndPricingNew.Persistence/Scripts/Seeders/PrintFormat.sql
+++ ProductsAndPricingNew.Persistence/Scripts/Seeders/PrintFormat.sql
@@ -1,8 +1,4 @@
-SET IDENTITY_INSERT [PricingRef].[PrintFormat] ON;
-
 INSERT INTO [PricingRef].[PrintFormat] ([Id], [Name], [IsDeleted])
 VALUES
     (1, N'A4', 0),
     (2, N'Letter', 0);
-
-SET IDENTITY_INSERT [PricingRef].[PrintFormat] OFF;
